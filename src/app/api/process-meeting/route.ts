60da5f34dd4920a1382e1ce3446c59d87ed72f41
vishnu-git1524 <vishnuvenkat1524@gmail.com>
2024-12-12 17:04:25 +0530

    updated maxDuration

--- src/app/api/process-meeting/route.ts
+++ src/app/api/process-meeting/route.ts
@@ -10,7 +10,7 @@ const bodyParser = z.object({
     meetingId: z.string()
 })
 
-export const maxDuration = 300
+export const maxDuration = 60
 
 export async function POST(req: NextResponse) {
     const { userId } = await auth()
